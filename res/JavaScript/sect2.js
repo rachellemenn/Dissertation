f572688fb3e1f9d3aa99912333000d1f4eecd374
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2018-08-16 14:23:39 +0100

    Viz 7?

--- res/JavaScript/sect2.js
+++ res/JavaScript/sect2.js
@@ -639,7 +639,7 @@ function Visualization(fileName) {
             });
 
             // Define an arc generator. Note the radius is specified here, not the layout.
-            var arc = d3.svg.arc()
+            var arc = d3.arc()
                 .innerRadius(r / 2)
                 .outerRadius(r);
 
@@ -780,6 +780,7 @@ function Visualization(fileName) {
             new Loader("Assets/Data/Viz4.csv", this.DrawSimpleChart),
             new Loader("Assets/Data/Viz5.csv", this.DrawSimpleChart),
             new Loader("Assets/Data/Viz6.csv", this.Viz6),
+            new Loader(), this.Viz7,
             new Loader("Assets/Data/Viz8.csv", this.Viz8),
             new Loader("Assets/Data/Viz12.2.csv", this.Viz122),
             new Loader("Assets/Data/Viz12.1.csv", this.Viz121),
