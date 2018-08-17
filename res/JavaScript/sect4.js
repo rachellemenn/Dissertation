ddff78624037980125d49b1e6feb6fd6a32cfd7d
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2018-08-17 18:13:42 +0100

    bubblechart

--- res/JavaScript/sect4.js
+++ res/JavaScript/sect4.js
@@ -329,6 +329,108 @@ function Visualization(fileName) {
             });
     }
 
+    this.DrawBubble = function (data) {
+        function loadData(fileName)
+        {
+        return d3.csv(fileName);
+        }
+
+        var diameter = 600;
+        var color = [
+                "#0077b3","#0099e6","#1ab2ff","#66ccff", 
+        ]; 
+    
+        var bubble = d3.pack()
+            .size([diameter, diameter])
+            .padding(1.5);
+    
+        var svg = d3.select("body")
+            .append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .attr("class", "bubble");
+    
+        var dataPromise = loadData('Viz15.csv');
+        dataPromise.then(function(data){
+            data.forEach(function(d){
+                d.value = parseFloat(d.value);
+                console.log(data)
+                });
+                
+        var stratData = d3.stratify()
+        var nodes = stratData(data)
+            .sum(function (d) { return d.Value; });
+            //     .parentId(function(d) {return d["Religion"];})
+            //     .id(function(d){return d.religion;});
+            // var root = stratData(data);
+            // console.log(root);
+            // root          
+                
+                // .sort(function(a, b) { return b.Value - a.Value });
+    
+            // var nodes = root.descendants();
+            // console.log(nodes);
+          
+            // layout(root);
+    
+            // var nodes = d3.stratify()
+            //     .sum(function(d) { return d.Count; });
+    
+        var node = svg.selectAll(".node")
+            .data(bubble(nodes).descendants())
+            .enter()
+            .filter(function(d){
+                    return  !d.children
+            })
+            .append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+    
+        node.append("title")
+            .text(function(d) {
+                return d.Religion + ": " + d.Value;
+            });
+    
+        node.append("circle")
+            .attr("r", function(d) {
+                return d.r;
+            })
+            .style("fill", function(d,i) {
+                return color(i);
+            });
+    
+        node.append("text")
+            .attr("dy", ".2em")
+            .style("text-anchor", "middle")
+            .text(function(d) {
+                return d.data.Religion.substring(0, d.r / 3);
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", function(d){
+                return d.r/5;
+            })
+            .attr("fill", "white");
+    
+        node.append("text")
+            .attr("dy", "1.3em")
+            .style("text-anchor", "middle")
+            .text(function(d) {
+                return d.data.Count;
+            })
+            .attr("font-family",  "Gill Sans", "Gill Sans MT")
+            .attr("font-size", function(d){
+                return d.r/5;
+            })
+            .attr("fill", "white");
+    
+        d3.select(self.frameElement)
+            .style("height", diameter + "px");
+    
+        });
+    }
+
 
     this.Viz4 = function (data) {
 
